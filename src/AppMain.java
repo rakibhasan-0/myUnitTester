41d67854cee2a51e8b1d735c771c8706beb1e8d5
Rakib <Alvin15148@gmail.com>
2023-06-21 14:02:10 +0200

    Newly added

---
+++ src/AppMain.java
@@ -0,0 +1,16 @@
+public class AppMain {
+    public static void main(String[] args){
+
+        ButtonPanel buttonPanel = new ButtonPanel();
+        TextVisualization text = new TextVisualization(buttonPanel.getFrame(), buttonPanel.getButton());
+        ClearText clearText = new ClearText(buttonPanel.getFrame());
+
+        RunButtonAction run = new RunButtonAction(buttonPanel.getTextField(),buttonPanel.getButton(),
+                text.getTextArea());
+        ClearText cText = new ClearText(buttonPanel.getFrame());
+        ClearButtonAction cAction = new ClearButtonAction(cText.getClearButton(),run.getTextArea(),
+                run.getResultMessages());
+
+    }
+
+}
